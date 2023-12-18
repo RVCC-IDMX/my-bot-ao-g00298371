d1364da099420d4d5b30c107975ad2d714d87d21
Your Name <you@example.com>
2023-12-18 02:14:34 -0500

    Configured cowsay and guide with IDMX-291 peers.

---
+++ commands/utility/cowsay.js
@@ -0,0 +1,133 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable indent */
+const {
+  SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle,
+} = require('discord.js');
+const cowsay = require('cowsay');
+
+const charLimit = 500;
+const generateDummyText = () => {
+  let dummyText = '';
+  for (let i = 0; i < charLimit; i += 1) { dummyText += 'A'; }
+  return dummyText;
+};
+
+async function getCowList() {
+  function callback(error, names) {
+    if (error) {
+      console.error('Cowsay: A request to retrieve the cowlist could not be completed.');
+      return ['default'];
+    }
+    return names;
+  }
+  // Prohibited cows
+  function checkProhibited(string) {
+    try {
+      const exclusionList = [
+        'bishop',
+        'usa',
+        'ibm',
+        'kilroy',
+      ];
+      let stillTrue = true;
+
+      exclusionList.forEach((item) => {
+        if (item === string) {
+          stillTrue = false;
+        }
+      });
+
+      if (stillTrue && (`\`\`\`${cowsay.say({ text: generateDummyText(), f: string })}\`\`\``).length > 1900) {
+        stillTrue = false;
+      }
+      return stillTrue;
+    }
+    catch {
+      return false;
+    }
+  }
+  const cows = await cowsay.list(callback);
+  return cows.map((cowfile) => cowfile.split('.')[0])
+    .filter((name) => checkProhibited(name));
+}
+
+const cowList = getCowList();
+
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('cowsay')
+    .setDescription('Enter a short message to generate an ASCII art cow with a speech bubble')
+    .addStringOption((option) => option.setName('message')
+      .setDescription('A message to send via cow.')
+      .setMaxLength(charLimit))
+    .addStringOption((option) => option.setName('cow')
+      .setDescription('The cow to use')
+      .setAutocomplete(true))
+    .addStringOption((option) => option.setName('help')
+      .setDescription('Display a help dialog')
+      .addChoices({ name: 'Help me!', value: 'help' })),
+  async autocomplete(interaction) {
+    const focusedValue = interaction.options.getFocused();
+    const choices = await cowList;
+    const filtered = choices.filter((choice) => choice.startsWith(focusedValue || 'a'));
+    await interaction.respond(
+      filtered.map((choice) => ({ name: choice, value: choice })),
+    );
+  },
+  async exportCowList() {
+    const list = await cowList;
+    return list;
+  },
+  async execute(interaction) {
+    const choices = await cowList;
+    const message = interaction.options.getString('message');
+    const help = interaction.options.getString('help');
+    const cowFile = interaction.options.getString('cow');
+    if (cowFile && !choices.includes(cowFile)) {
+      await interaction.reply({ content: 'It looks like the cow you entered is invalid. To see a list of valid cows you can use, type /cowsay help', ephemeral: true });
+      return;
+    }
+    const cow = cowFile || 'default';
+    if (message) {
+      try {
+        const moo = cowsay.say({
+          text: message,
+          f: cow,
+        });
+
+        const delimiter = '\n';
+        const cleanLine = '';
+        const sanitizedMoo = moo.replaceAll('`', '\'').split(delimiter);
+        sanitizedMoo[0] = cleanLine;
+        sanitizedMoo[2] = cleanLine;
+        const formattedMoo = `\`\`\`\n${sanitizedMoo.join(delimiter)}\`\`\``;
+
+        await interaction.reply(formattedMoo);
+      }
+      catch (error) {
+        console.error(error);
+        await interaction.reply({ content: `It looks like there was an error processing your request: \n${error}`, ephemeral: true });
+      }
+    }
+    else if (help) {
+      const row = new ActionRowBuilder()
+        .addComponents(
+          new ButtonBuilder()
+            .setCustomId('cowsay-help')
+            .setLabel('Show me a list of "cows"!')
+            .setStyle(ButtonStyle.Primary),
+        );
+      await interaction.reply({
+        content: `To use Cowsay, you can enter a message of ${charLimit} `
+          + 'characters or less in the "message" field, and [optionally] enter a non-default "cow" in '
+          + 'the cow field. If you\'re not sure what "cows" I\'m talking about, go right ahead and press '
+          + 'the big blurple button below to display a list!',
+        components: [row],
+        ephemeral: true,
+      });
+    }
+  },
+
+};
---
+++ commands/utility/guide.js
@@ -0,0 +1,25 @@
+/* eslint-disable indent */
+const {
+  SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle,
+} = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('guide')
+    .setDescription('A guide post with buttons.'),
+  async execute(interaction) {
+    const row = new ActionRowBuilder()
+      .addComponents(
+        new ButtonBuilder()
+          .setCustomId('primary')
+          .setLabel('Click Here')
+          .setStyle(ButtonStyle.Primary),
+      ).addComponents(
+        new ButtonBuilder()
+          .setLabel('Link')
+          .setURL('https://github.com/RVCC-IDMX/my-bot-ao-g00298371')
+          .setStyle(ButtonStyle.Link),
+      );
+    await interaction.reply({ content: 'To open the guide, click the blue button below. To visit this project\'s repository on GitHub, click the gray link button to visit ', components: [row] });
+  },
+};
